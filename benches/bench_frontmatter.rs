159b046a203a0819842cab14beef73c9a8373e95
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-03-26 22:00:45 +0100

    fix(ssg): bench updates

--- benches/bench_frontmatter.rs
+++ benches/bench_frontmatter.rs
@@ -17,17 +17,45 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("extract front matter", |b| {
         b.iter(|| {
             let result = extract(black_box(content));
-            assert_eq!(
-                result,
-                (
-                    "My Title".to_owned(),
-                    "2000-01-01".to_owned(),
-                    "My Description".to_owned(),
-                    "foo, bar, baz".to_owned(),
-                    "/my-permalink".to_owned(),
-                    "page".to_owned(),
-                )
+            assert!(
+                result.contains_key("title"),
+                "title not found in front matter"
             );
+            assert!(
+                result.contains_key("date"),
+                "date not found in front matter"
+            );
+            assert!(
+                result.contains_key("description"),
+                "description not found in front matter"
+            );
+            assert!(
+                result.contains_key("keywords"),
+                "keywords not found in front matter"
+            );
+            assert!(
+                result.contains_key("permalink"),
+                "permalink not found in front matter"
+            );
+            assert!(
+                result.contains_key("layout"),
+                "layout not found in front matter"
+            );
+            assert!(
+                result.contains_key("---"),
+                "--- not found in front matter"
+            );
+            // assert_eq!(
+            //     result,
+            //     (
+            //         "My Title".to_owned(),
+            //         "2000-01-01".to_owned(),
+            //         "My Description".to_owned(),
+            //         "foo, bar, baz".to_owned(),
+            //         "/my-permalink".to_owned(),
+            //         "page".to_owned(),
+            //     )
+            // );
         })
     });
 }
