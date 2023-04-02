c22dbcdf8d29993662bdf8538b56b8a91a20595f
sebastienrousseau <sebastienrousseau@users.noreply.github.com>
2023-04-02 00:58:36 +0000

    Deploy documentation at f464bc6bf38b5a5a3af0f45e8269fe991b286968 f464bc6bf38b5a5a3af0f45e8269fe991b286968

--- source-files.js
+++
@@ -1,5 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"ssg":["",[],["cli.rs","compiler.rs","file.rs","frontmatter.rs","html.rs","json.rs","lib.rs","metatags.rs","navigation.rs","parser.rs","rss.rs","serve.rs","template.rs","utilities.rs"]],\
-"xtask":["",[],["main.rs"]]\
-}');
-createSourceSidebar();
