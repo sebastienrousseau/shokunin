e34542a835db28d83d3f505664eee17b2f97e683
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-05-31 20:53:16 +0100

    fix(ssg): updated unit tests

--- tests/test_json.rs
+++ tests/test_json.rs
@@ -1,14 +1,15 @@
 #[cfg(test)]
 mod tests {
-    use serde_json::json;
-    use serde_json::Map;
-    use serde_json::Value;
-    use ssg::json::manifest;
-    use ssg::options::ManifestOptions;
+    // Import necessary dependencies
+    use serde_json::{json, Value};
+    use ssg::{json::manifest, options::ManifestOptions};
 
     #[test]
     fn test_manifest_with_empty_options() {
+        // Create a default instance of ManifestOptions
         let options = ManifestOptions::default();
+
+        // Define the expected result as a raw string with consistent indentation
         let expected_result = r#"{
   "background_color": "",
   "description": "",
@@ -21,12 +22,21 @@ mod tests {
   "start_url": "",
   "theme_color": ""
 }"#;
+
+        // Generate the result using the manifest function
         let result = manifest(&options);
-        assert_eq!(result, expected_result);
+
+        // Parse both JSON strings into `serde_json::Value` and compare those
+        let expected_result_value: Value =
+            serde_json::from_str(expected_result).unwrap();
+        let result_value: Value =
+            serde_json::from_str(&result).unwrap();
+        assert_eq!(result_value, expected_result_value);
     }
 
     #[test]
     fn test_manifest_with_non_empty_options() {
+        // Create an instance of ManifestOptions with custom values
         let options = ManifestOptions {
             name: "My App".to_string(),
             short_name: "My App".to_string(),
@@ -35,6 +45,7 @@ mod tests {
             ..Default::default()
         };
 
+        // Define the expected result using the json! macro
         let expected_result = json!({
             "background_color": "",
             "description": "",
@@ -48,7 +59,10 @@ mod tests {
             "theme_color": "#ffffff"
         });
 
+        // Generate the result using the manifest function
         let result = manifest(&options);
+
+        // Assert that the deserialized result matches the expected result
         assert_eq!(
             serde_json::from_str::<Value>(&result).unwrap(),
             expected_result
