43fec74c3c8b5901f892e590f503beedff079e7d
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-07-12 22:57:44 +0100

    [Enhancement]: Optimise the Service Worker code so it more robust and following best practices #70

--- template/main.js
+++ template/main.js
@@ -7,10 +7,14 @@ class ServiceWorkerSetup {
     /**
      * Constructor for the ServiceWorkerSetup class.
      * Checks if service workers are supported and initiates registration if they are.
+     * If not, logs a warning to the console.
      */
     constructor() {
         if ("serviceWorker" in navigator) {
-            this.registerServiceWorker();
+            // Deferring service worker registration until after the page has loaded.
+            window.addEventListener('load', () => {
+                this.registerServiceWorker();
+            });
         } else {
             console.warn("Service workers are not supported by this browser");
         }
@@ -20,15 +24,70 @@ class ServiceWorkerSetup {
      * Method to register a service worker.
      * Logs a success message with the registration scope if registration succeeds,
      * or an error message if registration fails.
+     * Also checks for a new service worker installation and triggers an update if found.
      */
     registerServiceWorker() {
-        navigator.serviceWorker.register("/sw.js")
+        navigator.serviceWorker.register("/sw.js", {scope: './'})
             .then(registration => {
                 console.log("ServiceWorker registration successful with scope: ", registration.scope);
+
+                // If there's no controller, this page wasn't loaded via a service worker, so they're looking at the latest version.
+                // Exit early
+                if (!navigator.serviceWorker.controller) return;
+
+                // If there's a worker waiting, that means a new version has been found and the waiting worker can be updated
+                if (registration.waiting) {
+                    this.updateServiceWorker(registration.waiting);
+                    return;
+                }
+
+                // If there's a worker installing, track its progress. If it becomes "installed", we can update the service worker.
+                if (registration.installing) {
+                    this.trackInstallingWorker(registration.installing);
+                    return;
+                }
+
+                // If none of the above, then listen for new installing workers arriving.
+                // If one arrives, track its progress.
+                // If it becomes "installed", our service worker code can be updated.
+                registration.addEventListener('updatefound', () => {
+                    this.trackInstallingWorker(registration.installing);
+                });
             })
             .catch(error => {
                 console.error("ServiceWorker registration failed: ", error);
             });
+
+        // Ensure refresh is only called once.
+        // This works around a bug in "force update on reload".
+        let refreshing;
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+            if (refreshing) return;
+            window.location.reload();
+            refreshing = true;
+        });
+    }
+
+    /**
+     * Sends a 'skipWaiting' message to a service worker indicating that it should activate immediately.
+     * @param {ServiceWorker} worker - The service worker that should be updated.
+     */
+    updateServiceWorker(worker) {
+        worker.postMessage({action: 'skipWaiting'});
+    }
+
+    /**
+     * Listens for a state change on a service worker. If the state becomes 'installed',
+     * this means the service worker is ready to take over from the current one.
+     * Call updateServiceWorker() to trigger the new service worker to become active immediately.
+     * @param {ServiceWorker} worker - The service worker that is being installed.
+     */
+    trackInstallingWorker(worker) {
+        worker.addEventListener('statechange', () => {
+            if (worker.state === 'installed') {
+                this.updateServiceWorker(worker);
+            }
+        });
     }
 }
 
--- template/sw.js
+++ template/sw.js
@@ -2,17 +2,30 @@
 
 /**
  * Class to handle service worker caching and request serving.
+ * @param {string} offlinePage - The URL of the page to show when offline.
+ * @param {boolean} debugMode - If true, debug information will be logged to the console.
  */
 class ServiceWorkerManager {
     constructor(offlinePage, debugMode) {
-        this.cacheVersion = Date.now();
-        this.offlineCache = "offline-" + this.cacheVersion;
+        /**
+         * Cache version used for cache management.
+         */
+        this.CACHE_VERSION = 'v1';
+
+        /**
+         * List of cache keys this service worker cares about. Used to keep track and delete old caches.
+         */
+        this.CACHE_KEYS = [this.CACHE_VERSION];
+
         this.offlinePage = offlinePage;
         this.debugMode = debugMode;
 
         this.init();
     }
 
+    /**
+     * Initialize the service worker by setting up event listeners for install, activate, and fetch events.
+     */
     init() {
         self.addEventListener("install", this.onInstall.bind(this));
         self.addEventListener("activate", this.onActivate.bind(this));
@@ -32,72 +45,84 @@ class ServiceWorkerManager {
     /**
      * Event handler for the install event.
      * Caches the offline page and forces the waiting service worker to become the active service worker.
+     * @param {InstallEvent} event The install event.
      */
     onInstall(event) {
         this.debug("Installing Service Worker");
-        event.waitUntil(this.cacheOfflinePage().then(() => self.skipWaiting()));
+        event.waitUntil(
+            this.cacheOfflinePage().then(() => self.skipWaiting()).catch((error) => {
+                this.debug(`Install event error: ${error}`);
+            })
+        );
     }
 
     /**
      * Event handler for the activate event.
      * Clears old caches and takes control of all clients.
+     * @param {ExtendableEvent} event The activate event.
      */
     onActivate(event) {
         this.debug("Activating Service Worker");
-        event.waitUntil(this.clearOldCaches().then(() => self.clients.claim()));
+        event.waitUntil(
+            this.clearOldCaches().then(() => self.clients.claim()).catch((error) => {
+                this.debug(`Activate event error: ${error}`);
+            })
+        );
     }
 
     /**
      * Event handler for the fetch event.
      * Serves requests from the cache if possible, otherwise fetches from the network.
      * Caches successful network responses.
+     * @param {FetchEvent} event The fetch event.
      */
     onFetch(event) {
         const request = event.request;
-        if (request.url.startsWith(self.location.origin) && request.url.startsWith("http")) {
-            if (request.method === "GET") {
-                event.respondWith(
-                    // Fetch response from cache or network.
-                    caches.match(request).then(cachedResponse => {
-                        // Return cached response if available.
-                        if (cachedResponse) {
-                            this.debug(`Fetching ${request.url}`);
-                            this.debug(`Found in cache: ${request.url}`);
-                            return cachedResponse;
+        if (request.url.startsWith(self.location.origin) && request.url.startsWith("http") && request.method === "GET") {
+            event.respondWith(
+                caches.match(request).then(cachedResponse => {
+                    if (cachedResponse) {
+                        this.debug(`Fetching ${request.url}`);
+                        this.debug(`Found in cache: ${request.url}`);
+                        return cachedResponse;
+                    }
+                    
+                    this.debug(`Going to network: ${request.url}`);
+                    return fetch(request).then(response => {
+                        if (response && response.ok) {
+                            this.debug(`Saving in cache: ${request.url}`);
+                            return caches.open(this.CACHE_VERSION).then(cache => {
+                                cache.put(request, response.clone());
+                                return response;
+                            });
                         }
+                        return response;
+                    }).catch(error => this.handleFetchError(request, error));
+                })
+            );
+        } else {
+            this.debug("Ignoring non-GET request");
+        }
+    }
 
-                        this.debug(`Going to network: ${request.url}`);
-                        return fetch(request).then(response => {
-                            // Cache response if request was successful.
-                            if (response && response.ok) {
-                                this.debug(`Saving in cache: ${request.url}`);
-                                const clonedResponse = response.clone();
-                                return caches.open(this.offlineCache).then(cache => {
-                                    cache.put(request, clonedResponse);
-                                    return response;
-                                });
-                            }
-
-                            return response;
-                        }).catch(error => {
-                            this.debug(`Offline and no cache for: ${request.url}: ${error}`);
-                            // Return offline page or offline image if navigation or image request fails.
-                            if (request.mode === "navigate") {
-                                this.debug("Showing offline page");
-                                return caches.match(this.offlinePage);
-                            } else if (request.headers.get("Accept").includes("image")) {
-                                return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {
-                                    headers: {
-                                        "Content-Type": "image/svg+xml"
-                                    }
-                                });
-                            }
-                        });
-                    })
-                );
-            } else {
-                this.debug("Ignoring non-GET request");
-            }
+    /**
+     * Handle fetch errors, such as failing to retrieve a resource from the cache or network.
+     * For navigation requests, an offline page is shown.
+     * For image requests, an offline image is shown.
+     * @param {Request} request The failed request.
+     * @param {Error} error The error that caused the fetch to fail.
+     */
+    handleFetchError(request, error) {
+        this.debug(`Offline and no cache for: ${request.url}: ${error}`);
+        if (request.mode === "navigate") {
+            this.debug("Showing offline page");
+            return caches.match(this.offlinePage);
+        } else if (request.headers.get("Accept").includes("image")) {
+            return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {
+                headers: {
+                    "Content-Type": "image/svg+xml"
+                }
+            });
         }
     }
 
@@ -107,19 +132,19 @@ class ServiceWorkerManager {
      */
     cacheOfflinePage() {
         this.debug("Cache offline page");
-        return caches.open(this.offlineCache).then(cache =>
+        return caches.open(this.CACHE_VERSION).then(cache =>
             cache.addAll([this.offlinePage])
-        ).catch(this.debug);
+        );
     }
 
     /**
-     * Deletes all caches that do not match the current offline cache.
+     * Deletes all caches that do not match the current cache version.
      * @returns {Promise} A promise that resolves once old caches are deleted.
      */
     clearOldCaches() {
         this.debug("Clean old caches");
         return caches.keys().then(keys =>
-            Promise.all(keys.filter(key => key !== this.offlineCache).map(key => caches.delete(key)))
+            Promise.all(keys.filter(key => !this.CACHE_KEYS.includes(key)).map(key => caches.delete(key)))
         );
     }
 }
